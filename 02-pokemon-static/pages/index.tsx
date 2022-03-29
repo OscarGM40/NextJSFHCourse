2d24b609a2af388d2ed7197f336291f6ed316929
Oscar <oscargm28@hotmail.com>
2022-03-29 20:38:35 +0200

    next ui impl

---
+++ 02-pokemon-static/pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import { Button } from '@nextui-org/react';
+
+const HomePage: NextPage = () => {
+  return (
+    <>
+      <Button color="gradient">Click me</Button>;
+    </>
+  );
+};
+
+export default HomePage;
