8b4e8bbdaa2ac08454f8c527398958112616f113
Oscar <oscargm28@hotmail.com>
2022-04-09 13:09:26 +0200

    favoritos added

--- 02-pokemon-static/components/ui/Navbar.tsx
+++ 02-pokemon-static/components/ui/Navbar.tsx
@@ -1,4 +1,5 @@
-import { Spacer, Text, useTheme } from "@nextui-org/react";
+import NextLink from "next/link";
+import { Link, Spacer, Text, useTheme } from "@nextui-org/react";
 import Image from "next/image";
 
 export const Navbar = () => {
@@ -22,18 +23,27 @@ export const Navbar = () => {
         width={70}
         height={70}
       />
-      <Text color="white" h2>
-        P
-      </Text>
-      <Text color="white" h3>
-        okémon
-      </Text>
-
+      <NextLink href="/" passHref>
+        <Link>
+          <Text color="white" h2>
+            P
+          </Text>
+          <Text color="white" h3>
+            okémon
+          </Text>
+        </Link>
+      </NextLink>
       {/* si son componentes de NextUI se recomienda usar el atributo css en vez de style para dar estilos en linea */}
       <Spacer css={{ flex: 1 }} />
-      <Text color="white" h3>
-        Favoritos
-      </Text>
+
+      {/* parece que es necesario passHref para que realmente le llegue el valor del href.Luego es obligatorio siempre?? */}
+      <NextLink href="/favorites" passHref>
+        <Link>
+          <Text color="white" h3>
+            Favoritos
+          </Text>
+        </Link>
+      </NextLink>
     </div>
   );
 };
--- 02-pokemon-static/pages/pokemon/[id].tsx
+++ 02-pokemon-static/pages/pokemon/[id].tsx
@@ -1,18 +1,94 @@
+import { Button, Card, Container, Grid, Image, Text } from "@nextui-org/react";
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
-import { useRouter } from "next/router";
+import { useState } from "react";
 import { Layout } from "../../components/layouts";
 import { PokemonResponse } from "../../interfaces";
+import { localFavorites } from "../../utils";
 import { pokeApi } from "../api";
 
 interface Props {
   pokemon: PokemonResponse;
 }
 const PokemonPage: NextPage<Props> = ({ pokemon }) => {
-  const router = useRouter();
-  
-  console.log(pokemon);
+  const [isInFavorites, setIsInFavorites] = useState(
+    typeof window !== "undefined" &&
+      localFavorites.existsInFavorites(pokemon.id)
+  );
 
-  return <Layout title="PokemonPage">Temporal mokete</Layout>;
+  const onToggleFavorite = () => {
+    localFavorites.toggleFavorite(pokemon.id);
+    setIsInFavorites(!isInFavorites);
+  };
+  // console.log(typeof window);
+
+  return (
+    <Layout title={pokemon.name}>
+      <Grid.Container css={{ marginTop: "5px" }} gap={2}>
+        <Grid xs={12} sm={4}>
+          <Card hoverable css={{ paddding: "30px" }}>
+            <Card.Body>
+              <Card.Image
+                src={
+                  pokemon.sprites.other?.dream_world.front_default ||
+                  "no-image.png"
+                }
+                alt={pokemon.name}
+                width="100%"
+                height="200px"
+              />
+            </Card.Body>
+          </Card>
+        </Grid>
+        <Grid xs={12} sm={8}>
+          <Card hoverable css={{ paddding: "30px" }}>
+            <Card.Header
+              css={{ display: "flex", justifyContent: "space-between" }}
+            >
+              <Text h1 transform="capitalize">
+                {pokemon.name}
+              </Text>
+              <Button
+                color="gradient"
+                ghost={!isInFavorites}
+                onClick={onToggleFavorite}
+              >
+                {isInFavorites ? "En Favoritos" : "Guardar en Favoritos"}
+              </Button>
+            </Card.Header>
+            <Card.Body>
+              <Text size={30}>Sprites:</Text>
+              <Container direction="row" display="flex">
+                <Image
+                  src={pokemon.sprites.front_default}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={pokemon.sprites.back_default}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={pokemon.sprites.front_shiny}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={pokemon.sprites.back_shiny}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+              </Container>
+            </Card.Body>
+          </Card>
+        </Grid>
+      </Grid.Container>
+    </Layout>
+  );
 };
 /* getStaticPaths no funciona sin getStaticProps */
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
